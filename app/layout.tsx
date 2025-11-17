d8f10f27f5c850fd99acc3767eb88b3d60a472b9
Liam Ross <liamross.code@gmail.com>
2025-11-17 12:05:53 -0800

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { Github, Linkedin, Twitter } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -107,6 +108,7 @@ export default function RootLayout({
             <main className="mx-auto w-full max-w-[80ch]">{children}</main>
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
