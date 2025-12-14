4a1ba12e2747563570d3187867ca1980ca00651e
Liam Ross <liamross.code@gmail.com>
2025-12-14 12:40:14 -0800

    feat: implement dynamic blog post redirection and update next.config.ts

--- app/[blog]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect, RedirectType } from 'next/navigation';
-
-// Redirect to the blog post.
-export default async function BlogPage({ params }: { params: Promise<{ blog: string }> }) {
-  const { blog } = await params;
-  return redirect(`/blog/${blog}`, RedirectType.replace);
-}
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import { getAllPosts } from "@/lib/blog";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
@@ -7,6 +8,14 @@ const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   reactCompiler: true,
+
+  redirects: async () => [
+    ...(await getAllPosts()).map((post) => ({
+      source: `/${post.slug}`,
+      destination: `/blog/${post.slug}`,
+      permanent: true,
+    })),
+  ],
 };
 
 const withMDX = createMDX({
